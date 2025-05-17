648b2d7b1ac9119a67186c12d8474cc63893493d
mr-mutahir0001 <mutahirzafar508@gmail.com>
2025-05-17 11:33:40 +0500

    Pizza hut website

---
+++ src/funtion.js
@@ -0,0 +1,7 @@
+let toggler = document.querySelector("#toggler-btn");
+console.log(toggler);
+const hidden = document.querySelectorAll("#content-collapse");
+console.log(hidden);
+toggler.addEventListener("click", () =>{
+    console.log("button clicked")
+});
